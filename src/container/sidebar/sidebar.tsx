dd70c7bbc6f8e6b2b44ad1dd84a915002ddd0ac5
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-09-20 18:47:10 -0300

    show side menu item by role

--- src/container/sidebar/sidebar.tsx
+++ src/container/sidebar/sidebar.tsx
@@ -22,8 +22,10 @@ export const SideContainer = () => {
           return <SideContainerItem key={element[1]} href={`#/${element[2]}`} ><Tooltip data-tip={element[0].replaceAll('_', ' ')}><Icon name={element[1]} /></Tooltip><p>{UriScreenFormat(element[2])}</p></SideContainerItem>
         })} */}
         <SideContainerItem href={`#/${'host'}`} ><Tooltip data-tip="tooltip host"><Icon name="cpu-fill" /></Tooltip><p>{UriScreenFormat("host")}</p></SideContainerItem>
-        {getRoles().includes("ROLE_ADMIN") || getRoles().includes("ROLE_MODERATOR") &&
-          <SideContainerItem href={`#/${'useEntity'}`} ><Tooltip data-tip="tooltip user"><Icon name="people-circle" /></Tooltip><p>{UriScreenFormat("user")}</p></SideContainerItem>
+        {getRoles().includes("ROLE_ADMIN") || getRoles().includes("ROLE_MODERATOR") ?
+          <SideContainerItem href={`#/${'useEntity'}`} ><Tooltip data-tip="tooltip user"><Icon name="people-circle" /></Tooltip><p>{UriScreenFormat("userEntity")}</p></SideContainerItem>
+          :
+          <></>
         }
         {getRoles().includes("ROLE_ADMIN") &&
           <SideContainerItem href={`#/${'role'}`} ><Tooltip data-tip="tooltip role"><Icon name="chat-quote-fill" /></Tooltip><p>{UriScreenFormat("role")}</p></SideContainerItem>
